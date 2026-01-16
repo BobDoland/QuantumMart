48b69533575dc6ef9d03e57502e091fde6c63d3e
Matthew Cumbo <mattcumbo99@gmail.com>
2026-01-15 18:10:34 -0600

    Http stuff

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+@Injectable({ providedIn: 'root' })
 export class App {
+  private http = inject(HttpClient);
+
+  private readonly apiUrl = "http://localhost:8080/sample";
   protected readonly title = signal('QuantumMart');
+
+  springText = "";
+
+  fetchText(): void {
+    console.log("Clicked.");
+
+    this.http.get(this.apiUrl, { responseType: 'text' as const }).subscribe(data => {
+      this.springText = data
+
+      console.log("Data is " + data);
+    });
+  } 
 }
